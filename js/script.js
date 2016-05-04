acdb0647d18fdf383c7ac2bdfc9471033a459bca
benjaminmodayil <benjaminmodayil@cedarville.edu>
2016-05-04 13:05:26 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// $(document).ready(function() {
+//
+// 	if ($('p.oneStar').hasClass('oneStar')) {
+// 		$('p.oneStar').on("click", function() {
+// 			$('p.oneStar').removeClass('oneStar').addClass('twoStar');
+// 		});
+// 	}
+// 	 if ($('p.twoStar').hasClass('twoStar')) {
+// 		$('p.twoStar').on("click", function() {
+// 			$('p.twoStar').removeClass('twoStar').addClass('threeStar');
+// 		});
+// 	}
+// 	// else if ($('p.threeStar').hasClass('threeStar')) {
+// 	// 	$('p.threeStar').on("click", function() {
+// 	// 		$(this).removeClass('threeStar').addClass('fourStar');
+// 	// 	} );
+// 	// }
+// 	// else if ($('p.fourStar').hasClass('fiveStar')) {
+// 	// 	$('p.fourStar').on("click", function() {
+// 	// 		$(this).removeClass('fourStar').addClass('fiveStar');
+// 	// 	} );
+// 	// }
+// 	// else if ($('p.fiveStar').hasClass('fiveStar')) {
+// 	// 	$('p.fiveStar').on("click", function() {
+// 	// 		$(this).removeClass('fiveStar').addClass('oneStar');
+// 	// 	} );
+// 	// }
+//
+// })
